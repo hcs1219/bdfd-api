d32a0275b9e8c5bac2421589bc4bbc864c21356c
hcs1219 <87796473+hcs1219@users.noreply.github.com>
2023-10-30 16:44:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.use('/api/discord', (req, res) => {
     axios.get(`https://discord.com/api${endpoint.join('/')}`, {
       validateStatus: () => true,
       headers: {
-        Authorization: `Bot ${process.env.TOKEN}`
+        Authorization: `Bot MTE2ODQ2OTMyOTE0MzE1Njc3Nw.GYPxqf.gljY2OFdL-RWS_K8iWYAaltLrfv4Motles3-S4`
       }
     }).then(x => {
       res.set('Content-Type', 'application/json').send(JSON.stringify(x.data, null, 2));
